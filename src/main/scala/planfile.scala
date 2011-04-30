c62e695f71c194101b6230c84c6f38a7bb670db4
Patrick Boe <patrickboe@gmail.com>
2011-04-30 10:24:35 -0400

    opening plan stream lazily

--- src/main/scala/planfile.scala
+++ src/main/scala/planfile.scala
@@ -1,9 +1,9 @@
-package thimblr
+package org.thimblr.io
 
 import java.io._
 
-object Planfile {
-  def streamer(path: String)() = new FileReader(path)
+object Local {
+  def streamer(path: String) = () => new FileReader(path)
 }
 
 // vim: set sw=2 set softtabstop=2 et:
--- src/test/scala/planfile.i.scala
+++ src/test/scala/planfile.i.scala
@@ -3,24 +3,35 @@ package org.thimblr.test.model
 import org.scalatest.WordSpec
 import org.scalatest.matchers.ShouldMatchers
 import java.io.FileWriter
-import thimblr.Planfile._
+import org.thimblr.io._
 
 class PlanFileSpec extends WordSpec with ShouldMatchers {
   "streamer" when {
     "called with a string argument" should {
-      "return a function that streams from the file in that path" in {
-        val planStream = streamer("src/test/resources/testplans/.plan.simple")()
-        val actual = Stream
-                      .continually(planStream.read)
-                      .takeWhile(-1 !=)
-                      .map(_.toChar)
-                      .mkString
+      "return a lazy stream from the file in that path" in {
+        val path = "src/test/resources/testplans/.plan.simple"
+        val overwriter = new java.io.FileWriter(path)
+        try {
+          overwriter.write(
+"""This file
+Has three lines
+Of text."""
+          )
+        } finally { overwriter.close() }
+        val makeStream = Local.streamer(path)
+        val appender = new java.io.FileWriter(path,true)
+        try { appender.write("YEAAAAAAAAAA!") } finally { appender.close() }
         val expected =
 """This file
 Has three lines
-Of text.
-"""
+Of text.YEAAAAAAAAAA!"""
+        lazy val planStream = makeStream()
         try {
+          val actual = Stream
+                        .continually(planStream.read)
+                        .takeWhile(_ != -1)
+                        .map(_.toChar)
+                        .mkString
           assert(actual == expected,
             "\n" + actual + " should have been\n" + expected + " but was not.")
         } finally {
