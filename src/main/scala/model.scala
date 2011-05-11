14decd02689d91a413b06df23281e8d9ce05ca8e
Patrick Boe <patrickboe@gmail.com>
2011-05-11 00:13:39 -0400

    added license

--- src/main/scala/model.scala
+++ src/main/scala/model.scala
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2011, Patrick Boe
+ * =========================== 
+ * This program is distributed under the terms of the GNU General Public License.
+ *
+ * This file is part of Thimblus.
+ * 
+ * Thimblus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Thimblus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Thimblus.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.thimblus.model
 
 import scala.swing._
